39298976d5dc44d18ff4564494141f322d8e4c01
MaissyTsara <maissypermata@gmail.com>
2025-02-24 00:59:57 +0700

    add refactor code to master branch

---
+++ src/main/java/com/apiautomation/constants/Constants.java
@@ -0,0 +1,29 @@
+package com.apiautomation.constants;
+
+public class Constants {
+    /*
+     * 169.10.11.23
+     * URL
+     * 
+     * mvn -test production
+     */
+  
+    public static final String BASE_URL = "https://api.restful-api.dev";
+
+    // public String BASE_URL = "https://api.restful-api.dev";
+    
+    // String env, BASE_URL1;
+
+    // public void setBaseURL(){
+    //     if ("production".equals(env)){
+    //         BASE_URL1 = "https://api.restful-api.dev";
+    //     }else{
+    //         BASE_URL1 = "https://api.restful-api-staging.dev";
+    //     }
+    // }
+
+    // public String getBaseUrl(){
+    //     return BASE_URL;
+    // }
+
+}
---
+++ src/test/java/apiengine/Endpoints.java
@@ -0,0 +1,114 @@
+package apiengine;
+
+import com.apiautomation.constants.Constants;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Endpoints {
+
+    RequestSpecification requestSpecification;
+    Boolean boolean1;
+    // Constants constants;
+
+    public static String idObject;
+
+    public Endpoints(){
+        RestAssured.baseURI = Constants.BASE_URL;
+        requestSpecification = RestAssured
+                               .given()
+                               .log()
+                               .all();
+    }
+
+    public void noReturn(){
+
+    }
+
+    public Boolean bool(){
+        return boolean1;
+    }
+
+    public Response getAllObjects(String path){
+        // RestAssured.baseURI = "https://api.restful-api.dev";
+        
+        // requestSpecification = RestAssured
+        //                         .given();
+
+        Response response = requestSpecification
+                            // .log()
+                            // .all()
+                            .when()
+                                .get(path);
+        return response;
+        
+    }
+
+    public Response addObject2(String path, String json){
+        // RestAssured.baseURI = "https://api.restful-api.dev";
+        
+        // requestSpecification = RestAssured
+        //                        .given();
+
+        Response response = requestSpecification
+                            // .log()
+                            // .all()
+                            .pathParam("path", "objects")
+                            .body(json)
+                            .contentType("application/json")
+                            .when()
+                                .post("{path}");  
+        
+        System.out.println("Response after adding object : " + response.asPrettyString());
+        
+        JsonPath jsonPath = response.jsonPath();
+        // idObject = jsonPath.getString("data.id");
+        String newId = jsonPath.getString("id");
+
+        if (newId.isEmpty()){
+            throw new RuntimeException("Failed to retrieve ID from response");
+        }
+
+        idObject = newId;
+        System.out.println("Extracted ID : " + newId);
+
+
+        // System.out.println("Saved ID: " + idObject);
+
+        
+
+        return response;      
+    }
+
+    public Response getSingleObject2(String objects){
+
+        // RestAssured.baseURI = "https://api.restful-api.dev";
+        
+        // requestSpecification = RestAssured
+        //                        .given();
+
+        Response response = requestSpecification
+                            // .log()
+                            // .all()
+                            .pathParam("path", "objects")
+                            .pathParam("idObject", idObject)
+                            // .pathParam("idObject", idObject)
+                            .when()
+                               .get("{path}/{idObject}");
+    
+        return response;
+    }
+
+    // public static String getSavedId(){
+    //     return idObject;
+    // }
+
+    public static String getSavedId() {
+        System.out.println("Fetching saved ID: " + idObject);
+        return idObject;
+    }
+    
+
+}
